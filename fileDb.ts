98227d5d4d542e25a61feaf7efeab02be4c6bdec
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-08-03 22:01:48 +0400

    class 74 DB

---
+++ fileDb.ts
@@ -0,0 +1,34 @@
+import {promises as fs} from "fs";
+import {randomUUID} from "crypto";
+import {IProduct, ProductWithoutId} from "./types";
+const fileName = "./db.json";
+let data:IProduct[] = [];
+
+const fileDb = {
+    async init(){
+        try{
+            const fileContents =  await fs.readFile(fileName);
+            data = JSON.parse(fileContents.toString());
+        }catch(e){
+            data = []
+        }
+    },
+    async getItems(){
+        return data
+    },
+    async addItem(item: ProductWithoutId){
+        const product:IProduct = {
+            ...item,
+            id:randomUUID()
+        }
+        data.push(product);
+        await this.save();
+        return product
+
+    },
+    async save(){
+        await fs.writeFile(fileName, JSON.stringify(data));
+    }
+}
+
+export default fileDb;
