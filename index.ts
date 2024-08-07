a2df1ca8db7526e8ebb6b95f6daf858fde7ccc5f
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-08-07 18:01:13 +0400

    added back and front for class 75

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import productsRouter from "./routers/products";
 import fileDb from "./fileDb";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors())
+
 // MiddleWare
 app.use(express.json());
-app.use('/products', productsRouter)
+app.use('/products', productsRouter);
 
 
 const run = async ()=>{
